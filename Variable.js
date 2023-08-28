e02c5c59f78d5e535f0595c3e58fe764f0550e1c
codingwolf11 <133812892+codingwolf11@users.noreply.github.com>
2023-08-28 17:10:56 -0400

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,17 @@
+// I Declared the variable person then  i assigned the value perla to the variable person.
+
+var person;
+var person = "Perla"
+console.log("Hello",person); 
+ //then i printed the vaule of variable person using console log.
+
+ 
+ /*
+ example of changing values.
+ 
+ var greeting = "Hello"
+  console.log(greeting,person)
+  greeting = 'hi'
+  person = 'James'
+  console.log(greeting,person)
+ */
